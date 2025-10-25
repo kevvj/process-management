1ee728a39f21570911cf5eedd4775de73dbb83c1
kevvj <kevinjvillalba774+1@gmail.com>
2025-10-25 14:42:56 -0500

    Update processes.java

--- processes.java
+++ processes.java
@@ -1,5 +1,5 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
@@ -11,37 +11,38 @@ public class processes {
     public process[] getProcesses(int L) {
         process[] pl = new process[L];
         int j = 0;
+
         try {
-            Process p = Runtime.getRuntime().exec("tasklist /V /FO CSV");
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String[] cmd = {"tasklist", "/V", "/FO", "CSV"};
 
-            while (j < L) {
-                String line = reader.readLine();
+            Process p = Runtime.getRuntime().exec(cmd);
 
-                if (line == null) {
-                    break;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+
+                while (j < L) {
+                    String line = reader.readLine();
+
+                    if (line == null) {
+                        break;
+                    }
+
+                    String[] info = line.replace("\"", "").split(",");
+
+                    if (info.length >= 5) {
+                        String name = info[0];
+                        String processId = info[1];
+                        String user = info[6];
+                        String description = info[0];
+
+                        pl[j] = new process(description, name, processId, user);
+                        System.out.println(info[6]);
+                        j = j + 1;
+                    }
                 }
-
-                String[] info = line.replace("\"", "").split(",");
-
-                if (info.length >= 5) {
-                    String name = info[0];
-                    String processId = info[1];
-                    String user = info[6];
-                    String description = info[0];
-
-                    pl[j] = new process(description, name, processId, user);
-                    System.out.println(info[6]);
-                    j = j + 1;
-
-                }
-
             }
 
-            reader.close();
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Error: " + e);
         }
 
