696c5337d33e68a4259b23b04a415cbef331059d
Kevin Tapias <153977239+kevvj@users.noreply.github.com>
2025-10-27 15:51:06 -0500

    Fix variable naming case for Controller instance

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Controller C = new Controller();
+        controller C = new controller();
         
         C.start();
     }
